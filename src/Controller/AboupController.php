6036d64c51a9404f813fcf0310330100613ba701
SkillAlex1984 <dd050484fav1@gmail.com>
2017-12-05 21:35:20 +0200

    Homework symfony

---
+++ src/Controller/AboupController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MY
+ * Date: 05.12.2017
+ * Time: 20:03
+ */
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class AboupController extends Controller
+{
+    /**
+     * @Route("/about", name = "about_show")
+     */
+    public function show(SessionInterface $session)
+    {
+        $url = $this->generateUrl('category_show', [
+            'slug' => 'notebooks',
+            'param'=>'getparam',
+        ], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return $this->render('about/show.html.twig', [
+            'notebooksUrl'=>$url,
+            'lastCategory'=>$session->get('lastVisitCtegory'),
+        ]);
+    }
+
+    /**
+     * @Route("to-about")
+     */
+    public function redirectToShow ()
+    {
+        return $this->redirectToRoute('about_show');
+    }
+}
---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MY
+ * Date: 05.12.2017
+ * Time: 19:33
+ */
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Flex\Response;
+
+class CategoryController extends Controller
+{
+    /**
+     * @Route("/category/{slug}/{page}", name="category_show", requirements={"page": "\d+"})
+     *
+     * @param $slug
+     * @param $page
+     * @param $session
+     *
+     * @return Response
+     *
+     */
+    public function show ($slug, $page = 1, SessionInterface $session, Request $request)
+    {
+        $session->set('lastVisitCtegory', $slug);
+        $param = $request->query->get('param');
+
+        return $this->render('category/show.html.twig', ['slug'=>$slug, 'page'=>$page, 'param'=>$param]
+        );
+    }
+
+    /**
+     * @Route("/message", name = "category_message")
+     */
+    public function message (SessionInterface $session)
+    {
+        $this->addFlash('notice', 'You message');
+        $lastCategory = $session->get('lastVisitCtegory');
+
+        return $this->redirectToRoute('category_show', ['slug' => $lastCategory]);
+    }
+
+    /**
+     * @Route("download", name = "category_download")
+     */
+    public function fileDownload ()
+    {
+        $response = new \Symfony\Component\HttpFoundation\Response();
+        $response->setContent('Test content');
+
+        return$response;
+    }
+
+}
